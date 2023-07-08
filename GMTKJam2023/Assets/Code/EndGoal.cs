008939a07bc465afd49bbac9d3303e3097276a59
Derpidius <82119250+DerpidiusBidoof@users.noreply.github.com>
2023-07-08 00:22:58 -0500

    enemy can take damage and die if hp drops to 0, found in end goal script and commented out
    variables enemyHealth and damage are for takeDamage, takeDamage is currently commented out in End Goal

--- GMTKJam2023/Assets/Code/EndGoal.cs
+++ GMTKJam2023/Assets/Code/EndGoal.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class EndGoal : MonoBehaviour
 {
+    public int damage;
     public EnemyHealth enemyHealth;
     // Start is called before the first frame update
     void Start()
@@ -23,6 +24,8 @@ public class EndGoal : MonoBehaviour
         {
             Destroy(gameObject);
             Debug.Log("GOAT IN");
+           // enemyHealth.takeDamage(damage);
+            //Debug.Log("Damage taken" + enemyHealth);
         }
     }
 }
