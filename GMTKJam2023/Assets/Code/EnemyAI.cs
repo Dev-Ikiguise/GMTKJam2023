0ae2684aac245831d49fd8c8947ee6a118eea334
Iki Pham <dev.ikiguise@gmail.com>
2023-07-09 00:10:51 -0500

    fucking animations

--- GMTKJam2023/Assets/Code/EnemyAI.cs
+++ GMTKJam2023/Assets/Code/EnemyAI.cs
@@ -8,6 +8,7 @@ public class EnemyAI : MonoBehaviour
 
     public Transform player;
     public Transform enemy;
+    public Animator ani;
 
 
     public float enemyMoveSpeed;
@@ -15,12 +16,14 @@ public class EnemyAI : MonoBehaviour
     public float doubleDashFromPlayer;
     public float decelerationRate;
 
+    public float currentFacing = -1f;
+
     //private Vector2 directionToFace;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-
+        ani = GetComponent<Animator>();
     }
 
     // Start is called before the first frame update
@@ -34,6 +37,18 @@ public class EnemyAI : MonoBehaviour
     {
         // Calculate the distance between player and enemy
         float distance = Vector3.Distance(player.position, enemy.position);
+
+        Vector2 playerDirectionLocal = enemy.transform.InverseTransformPoint(player.transform.position);
+
+        if (playerDirectionLocal.x < 0)
+        {
+            ani.SetFloat("h", 1);
+        }
+
+        if (playerDirectionLocal.x > 0)
+        {
+            ani.SetFloat("h", -1);
+        }
 
         if (distance <= proximityFromPlayer)
         {
@@ -49,6 +64,7 @@ public class EnemyAI : MonoBehaviour
         else
         {
             stopRunningAway();
+            ani.SetFloat("speed", 0);
         }
     }
 
@@ -64,8 +80,9 @@ public class EnemyAI : MonoBehaviour
     void moveAwayFromPlayer()
     {
         transform.position = Vector2.MoveTowards(this.transform.position, player.position, enemyMoveSpeed * -Time.deltaTime);
+        ani.SetFloat("speed", 1);
         //Vector2 directionToFace = new Vector2(transform.position.x - player.position.x, transform.position.y - player.position.y);
-       // transform.up = directionToFace;
+        // transform.up = directionToFace;
         //runAway();
     }
 
